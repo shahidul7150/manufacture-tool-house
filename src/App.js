91c9d012f8ba0c57e3903bcb79ff29a32da20219
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-22 00:58:48 +0600

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Pages/Dashboard/Dashboard";
+import Home from "./Components/Pages/Home/Home";
+import MyPortfolio from "./Components/Pages/MyPortfolio/MyPortfolio";
+import Purchase from "./Components/Pages/Purchase/Purchase";
+import Login from "./Components/Pages/Login/Login";
+import Navbar from "./Components/Shared/Navbar";
+import Blogs from "./Components/Pages/Blogs/Blogs";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React initial app created</h1>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/purchase" element={<Purchase/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/portfolio" element={<MyPortfolio/>}></Route>
+        <Route path="/blogs" element={<Blogs/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
     </div>
   );
 }
