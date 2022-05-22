6896bf5096e3486cd7fe70c87a1dfd8f7e32a5dc
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-22 11:14:10 +0600

    create ourtools section

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import ToolsSection from './ToolsSection';
 
 const Home = () => {
     return (
         <div>
-            <Banner/>
+            <Banner />
+            <ToolsSection/>
         </div>
     );
 };
---
+++ src/Components/Pages/Home/Tool.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Tool = ({ tool }) => {
+    const{name,img,price,description,minimum,quantity}=tool
+    return (
+        <div className='shadow-lg py-5 '>
+            <img src={img} alt="" />
+            <div className='px-4'>
+            <h1 className='text-xl font-bold'>{name}</h1>
+            <p>{description}</p>
+            <h3 className=' font-bold text-primary'>Price Unit: {price}</h3>
+            <p><small>Minimum Order: {minimum}</small></p>
+            <p className='text-secondary'><small>Available Unites:{quantity}</small></p>
+            <button className='btn bg-orange-600 outline-none border-0 mt-2'>book now</button>
+            </div>
+        </div>
+    );
+};
+
+export default Tool;
