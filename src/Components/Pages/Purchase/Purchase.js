442b1aa85b65b19b97a921c7b177fab7d123bd81
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-23 16:14:16 +0600

    create purchase page

--- src/Components/Pages/Purchase/Purchase.js
+++ src/Components/Pages/Purchase/Purchase.js
@@ -5,12 +5,33 @@ const Purchase = () => {
   const { purchaseId } = useParams();
   const [purchase, setPurchase] = useState({});
 
+    const [increase, setIncrease] = useState(0);
+    const [change,setChange]=useState('')
+  const increasehandle = () => {
+    const count = increase + 1;
+    setIncrease(count);
+  };
+  const decreasehandle = () => {
+    const count = increase - 1;
+    setIncrease(count);
+  };
+    console.log(increase);
+    
+
+    const changeValue=(event)=> {
+    setChange(event.target.value);
+}
   useEffect(() => {
     const url = `http://localhost:5000/product/${purchaseId}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setPurchase(data));
   }, []);
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+  };
+  const minimumQuantity = parseInt(purchase.quantity)-increase;
   return (
     <div className="m-14">
       <div class="card lg:card-side bg-base-100 shadow-xl">
@@ -20,35 +41,79 @@ const Purchase = () => {
         <div class="card-body justify-center">
           <h2 class="card-title">{purchase.name}</h2>
           <p className="flex-grow-0">{purchase.description}</p>
-          <p className="flex-grow-0 font-bold text-primary">Per Unit: {purchase.price} USD</p>
+          <p className="flex-grow-0 font-bold text-primary">
+            Per Unit: {purchase.price} USD
+          </p>
           {/* input      */}
-          <form>
+          <form onSubmit={formSubmit}>
             <div class="form-control w-full max-w-xs">
               <label class="label">
                 <span class="label-text">Quantity</span>
               </label>
 
-              <input
+              {/* <input
                 type="number"
                 placeholder="Type here"
                 class="input input-bordered w-full max-w-xs"
-              />
+              /> */}
+
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Enter amount</span>
+                </label>
+                <label class="input-group">
+                  <button
+                    onClick={increasehandle}
+                    className="bg-gray-300 px-5 text-2xl font-bold text-primary"
+                  >
+                    +
+                  </button>
+                  <input onChange={changeValue} type="text" name="name" id="" className='text-center' value={increase} />
+                  {increase < 1 ? (
+                    <button
+                      disabled
+                      className="bg-gray-300 px-5 text-2xl font-bold text-primary"
+                    >
+                      -
+                    </button>
+                  ) : (
+                    <button
+                      onClick={decreasehandle}
+                      className="bg-gray-300 px-5 text-2xl font-bold text-primary"
+                    >
+                      -
+                    </button>
+                  )}
+                </label>
+              </div>
+
               <label class="label">
                 <span class="label-text-alt text-primary">
                   Minimum {purchase.minimum} order
                 </span>
               </label>
               <label class="label">
-                <span class="label-text text-lime-600">Available Now: {purchase.quantity}</span>
+                <span class="label-text text-lime-600">
+                  Available Now: {minimumQuantity}
+                </span>
               </label>
             </div>
             {/* input end */}
             <div class="card-actions justify-start">
-              <input
-                class="btn btn-primary"
-                type="submit"
-                value="Add Purchase"
-              />
+              {increase < purchase.minimum ? (
+                <input
+                  disabled
+                  class="btn  btn-primary"
+                  type="submit"
+                  value="Add Purchase"
+                />
+              ) : (
+                <input
+                  class="btn  btn-primary"
+                  type="submit"
+                  value="Add Purchase"
+                />
+              )}
             </div>
           </form>
         </div>
